a453d67ffa1c3b61f426bca7a3619a034ec2560c
Szymon Paprocki <s.paprocki.vs@gmail.com>
2022-01-30 18:49:24 +0100

    Session and break time can be changed now

--- src/App.js
+++ src/App.js
@@ -1,131 +1,218 @@
-import './App.css';
+import "./App.css";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
-import { useState } from 'react';
-import { BsPlayFill, BsStopFill, BsPauseFill, BsArrowRepeat } from 'react-icons/bs';
+import { useState } from "react";
+import {
+  BsPlayFill,
+  BsStopFill,
+  BsPauseFill,
+  BsArrowRepeat,
+  BsFillCaretRightFill,
+  BsFillCaretLeftFill,
+} from "react-icons/bs";
 
 function App() {
-  const workTime = 15;
+  const [workTime, setWorkTime] = useState(1500);
+  const [pauseTime, setPauseTime] = useState(300);
   const [key, setKey] = useState(0);
   const [time, setTime] = useState(workTime);
   const [timerOn, setTimerOn] = useState(false);
-  const [active, setActive] = useState(false)
+  const [status, setStatus] = useState('Session')
 
   const renderTime = ({ remainingTime }) => {
     const minutes = Math.floor(remainingTime / 60);
     const seconds = remainingTime % 60;
 
     if (remainingTime === 0) {
-      return <button className="large-button"
-        onClick={() => {
-          setKey(prevKey => prevKey + 1)
-          setTime(workTime)
-          setTimerOn(true)
-        }}>
-        <BsArrowRepeat size="100" />
-      </button>
-    }
-
-    if (remainingTime === workTime) {
-
-      if (timerOn === false) {
-        return <button className="large-button" onClick={() => setTimerOn(true)}>
-          <BsPlayFill size="100" />
+      return (
+        <button
+          className="large-button"
+          onClick={() => {
+            setKey((prevKey) => prevKey + 1);
+            setTime(workTime);
+            setTimerOn(true);
+          }}
+        >
+          <BsArrowRepeat size="100" />
         </button>
-      }
+      );
+    }
 
-      else {
+    if (remainingTime === time) {
+      if (timerOn === false) {
+        return (
+          <button className="large-button" onClick={() => setTimerOn(true)}>
+            <BsPlayFill size="100" />
+          </button>
+        );
+      } else {
         return (
           <div className="timer">
-            <div className="value">{minutes}:{("0" + seconds).slice(-2)}</div>
+            <div className="status">{status}</div>
+            <div id="time-left" className="value">
+              {("0" + minutes).slice(-2)}:{("0" + seconds).slice(-2)}
+            </div>
             <div>
-              <BsPauseFill className="small-button"
+              <BsPauseFill
+                className="small-button"
                 size="30"
-                onClick={() => setTimerOn(false)} />
-              <BsStopFill className="small-button"
+                onClick={() => setTimerOn(false)}
+              />
+              <BsStopFill
+                className="small-button"
                 size="30"
-                onClick={() => { setKey(prevKey => prevKey + 1); }} />
+                onClick={() => {
+                  setKey((prevKey) => prevKey + 1);
+                }}
+              />
             </div>
           </div>
         );
       }
     }
 
-    if (remainingTime < workTime) {
+    if (remainingTime < time) {
       if (timerOn === false) {
         return (
           <div className="timer">
-            <div className="value">{minutes}:{("0" + seconds).slice(-2)}</div>
+            <div className="status">{status}</div>
+            <div id="time-left" className="value">
+              {("0" + minutes).slice(-2)}:{("0" + seconds).slice(-2)}
+            </div>
             <div>
-              <BsPlayFill className="small-button"
+              <BsPlayFill
+                className="small-button"
                 size="30"
-                onClick={() => setTimerOn(true)} />
-              <BsStopFill className="small-button"
+                onClick={() => setTimerOn(true)}
+              />
+              <BsStopFill
+                className="small-button"
                 size="30"
                 onClick={() => {
-                  setKey(prevKey => prevKey + 1)
-                  setTime(workTime)
-                  setTimerOn(false)
-                }} />
+                  setKey((prevKey) => prevKey + 1);
+                  setTime(workTime);
+                  setTimerOn(false);
+                }}
+              />
             </div>
           </div>
         );
-      }
-
-      else {
+      } else {
         return (
           <div className="timer">
-            <div className="value">{minutes}:{("0" + seconds).slice(-2)}</div>
+            <div className="status">{status}</div>
+            <div id="time-left" className="value">
+              {("0" + minutes).slice(-2)}:{("0" + seconds).slice(-2)}
+            </div>
             <div>
-              <BsPauseFill className="small-button"
+              <BsPauseFill
+                className="small-button"
                 size="30"
-                onClick={() => setTimerOn(false)} />
-              <BsStopFill className="small-button"
+                onClick={() => setTimerOn(false)}
+              />
+              <BsStopFill
+                className="small-button"
                 size="30"
                 onClick={() => {
-                  setKey(prevKey => prevKey + 1);
+                  setKey((prevKey) => prevKey + 1);
                   setTimerOn(false);
-                }} />
+                }}
+              />
             </div>
           </div>
         );
       }
     }
-  }
+  };
 
   const playSound = () => {
-    const audioTag = new Audio('https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3')
-    setActive(true)
-    setTimeout(() => setActive(false), 200)
-    audioTag.volume = 1
-    audioTag.currentTime = 0
-    audioTag.play()
-  }
+    const audioTag = new Audio(
+      "https://onlineclock.net/audio/options/default.mp3"
+    );
+    audioTag.volume = 1;
+    audioTag.currentTime = 0;
+    audioTag.play();
+  };
 
   return (
     <div className="App">
       <div className="timer-wrapper">
-        <div className="timer-header">
-          {'Pomodoro Clock'}
+        <div className="timer-header">{"Pomodoro Clock"}</div>
+        <div className="session-wrapper">
+          <div id="session-label">Session Length</div>
+          <div className="buttons-wrapper">
+            <BsFillCaretLeftFill
+              className="smaller-button"
+              size="20"
+              onClick={() => {
+                if (timerOn === false) {
+                  setWorkTime((prevworkTime) => prevworkTime - 60);
+                  setTime((prevTime) => prevTime - 60);
+                }
+              }}
+            />
+            <div>{(("0" + (workTime/60)).slice(-2))}</div>
+            <BsFillCaretRightFill
+              className="smaller-button"
+              size="20"
+              onClick={() => {
+                if (timerOn === false) {
+                  setWorkTime((prevworkTime) => prevworkTime + 60);
+                  setTime((prevTime) => prevTime + 60);
+                }
+              }}
+            />
+          </div>
         </div>
-        <CountdownCircleTimer className="circle-timer"
+
+        <div className="session-wrapper">
+          <div id="break-label">Break Length</div>
+          <div className="buttons-wrapper">
+            <BsFillCaretLeftFill
+              className="smaller-button"
+              size="20"
+              onClick={() => setPauseTime((prevpauseTime) => prevpauseTime - 60)}
+            />
+            <div>{(("0" + (pauseTime/60)).slice(-2))}</div>
+            <BsFillCaretRightFill
+              className="smaller-button"
+              size="20"
+              onClick={() => setPauseTime((prevpauseTime) => prevpauseTime + 60)}
+            />
+          </div>
+        </div>
+
+        <CountdownCircleTimer
+          className="circle-timer"
           key={key}
           isPlaying={timerOn}
           duration={time}
           size={250}
           strokeWidth={16}
-          colors={["#FCE4EC", "#F06292", "#D81B60", "#880E4F"]}
+          colors={["#F4BECF", "#EEA9BF", "#E994AE", "#E37F9D"]}
           colorsTime={[1500, 1000, 500, 0]}
-          onComplete={() => {
-            playSound();
-            return { shouldRepeat: false, delay: 1 };
-          }
-          }
+          onComplete={(duration) => {
+            if (duration === workTime) {
+              playSound();
+              setTime(pauseTime);
+              setTimerOn(true);
+              setKey((prevKey) => prevKey + 1);
+              setStatus('Break');
+              return { shouldRepeat: true, delay: 1 };
+
+            } else {
+              playSound();
+              setTime(workTime);
+              setTimerOn(false);
+              setKey((prevKey) => prevKey + 1);
+              setStatus('Session');
+              return { shouldRepeat: false, delay: 1 };
+            }
+          }}
         >
           {renderTime}
         </CountdownCircleTimer>
       </div>
     </div>
-
   );
 }
 
